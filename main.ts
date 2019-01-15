14df2ed873fd6ac047cdb588c176fb8618ea900d
glasspinnA <ozze-1995@hotmail.com>
2019-01-15 16:21:30 +0100

    possible to move snake

--- main.ts
+++ main.ts
@@ -1,74 +1,204 @@
 var snakeGame: SnakeGame;
+enum Direction {
+    LEFT, RIGHT, UP, DOWN
+}
+
+
+class Position {
+    xPostion: number;
+    yPosition: number;
+
+    constructor(xPosition: number, yPosition: number) {
+        this.xPostion = xPosition;
+        this.yPosition = yPosition;
+    }
+
+}
 
 
 class SnakeGame {
     private canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
     private gameSpeed: number = 100;
+    private direction: Direction;
+
+    private canvasHeight: number;
+    private canvasWidth: number;
+
+    private isGamePaused: boolean = false;
+
+    private SnakePosition: Position;
+    private food: Position;
 
     constructor(mCanvas: HTMLCanvasElement) {
         this.canvas = mCanvas;
-        this.context = this.canvas.getContext("2d");
-
+        this.context = mCanvas.getContext("2d");
+        this.canvasHeight = mCanvas.height;
+        this.canvasWidth = mCanvas.width;
     }
 
-    start() {
-        this.restart();
-        setInterval(() => snakeGame.loop(), this.gameSpeed);
+
+    private clearCanvas() {
+        this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     }
 
-    restart() {
 
+    public pauseGame() {
+        //clearInterval(this.game);
+        this.isGamePaused = !this.isGamePaused;
     }
 
-    loop() {
+    public start() {
+
+        this.SnakePosition = new Position(100, 100);
+        this.food = new Position(100, 164);
+
+        setInterval(() => {
+            if (!this.isGamePaused) {
+                snakeGame.loop();
+            }
+        }, this.gameSpeed);
+    }
+
+
+    private loop() {
+        this.move();
+
+
+
+        if (this.SnakePosition.xPostion == this.food.xPostion && this.SnakePosition.yPosition == this.food.yPosition) {
+            this.eat();
+        }
+        this.checkBorderCollision();
         this.draw();
     }
 
-    draw() {
-        console.log("draw");
+
+    private eat() {
+        console.log("eat");
+    }
+
+    private drawApple() {
+
+        this.context.fillStyle = "red";
+        this.context.fillRect(this.food.xPostion, this.food.yPosition, 32, 32);
+    }
+
+    private draw() {
+        this.clearCanvas();
+        this.drawApple();
+
 
         this.context.fillStyle = "green";
-        this.context.fillRect(100, 100, 32, 32);
+        this.context.fillRect(this.SnakePosition.xPostion, this.SnakePosition.yPosition, 32, 32);
+    }
 
+    private checkBorderCollision() {
+        if (this.SnakePosition.xPostion <= 8) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+
+        if (this.SnakePosition.xPostion >= this.canvasWidth - 32) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+
+        if (this.SnakePosition.yPosition >= this.canvasHeight - 32) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+
+        if (this.SnakePosition.yPosition <= 8) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+    }
+
+
+    private move() {
+        switch (this.direction) {
+            case Direction.LEFT:
+                this.SnakePosition.xPostion -= 32;
+                break;
+            case Direction.UP:
+                this.SnakePosition.yPosition -= 32;
+                break;
+            case Direction.RIGHT:
+                this.SnakePosition.xPostion += 32;
+                break;
+            case Direction.DOWN:
+                this.SnakePosition.yPosition += 32;
+                break;
+        }
+    }
+
+    public moveLeft() {
+        if (this.direction != Direction.RIGHT) {
+            this.direction = Direction.LEFT;
+        }
+    }
+
+    public moveUp() {
+        if (this.direction != Direction.DOWN) {
+            this.direction = Direction.UP;
+        }
+    }
+
+    public moveRight() {
+        if (this.direction != Direction.LEFT) {
+            this.direction = Direction.RIGHT;
+        }
+    }
+
+    public moveDown() {
+        if (this.direction != Direction.UP) {
+            this.direction = Direction.DOWN;
+        }
     }
 }
 
 
 
-function direction(event: KeyboardEvent) {
-    let key = event.keyCode;
+function keyPressed(event: KeyboardEvent) {
+    const KEYBOARD_BUTTON = event.keyCode;
     const KEY_LEFT: number = 37;
     const KEY_UP: number = 38;
     const KEY_RIGHT: number = 39;
     const KEY_DOWN: number = 40;
 
-    switch (key) {
+    const KEY_STOP_GAME: number = 80;
+
+    switch (KEYBOARD_BUTTON) {
         case KEY_RIGHT: {
-            console.log("right");
+            snakeGame.moveRight();
             break;
         }
         case KEY_DOWN: {
-            console.log("down");
+            snakeGame.moveDown();
             break;
         }
         case KEY_LEFT: {
-            console.log("left");
+            snakeGame.moveLeft();
             break;
         }
         case KEY_UP: {
-            console.log("up");
+            snakeGame.moveUp();
+            break;
+        }
+        case KEY_STOP_GAME: {
+            snakeGame.pauseGame();
             break;
         }
     }
 }
 
 window.onload = () => {
-    document.addEventListener("keydown", direction);
+    document.addEventListener("keydown", keyPressed);
 
     var canvas = <HTMLCanvasElement>document.getElementById('snakeCanvas');
     snakeGame = new SnakeGame(canvas);
-    //snakeGame.start();
+    snakeGame.start();
 };
 
 
--- outdir/main.js
+++ outdir/main.js
@@ -1,55 +1,155 @@
 "use strict";
 var snakeGame;
+var Direction;
+(function (Direction) {
+    Direction[Direction["LEFT"] = 0] = "LEFT";
+    Direction[Direction["RIGHT"] = 1] = "RIGHT";
+    Direction[Direction["UP"] = 2] = "UP";
+    Direction[Direction["DOWN"] = 3] = "DOWN";
+})(Direction || (Direction = {}));
+var Position = /** @class */ (function () {
+    function Position(xPosition, yPosition) {
+        this.xPostion = xPosition;
+        this.yPosition = yPosition;
+    }
+    return Position;
+}());
 var SnakeGame = /** @class */ (function () {
     function SnakeGame(mCanvas) {
         this.gameSpeed = 100;
+        this.isGamePaused = false;
         this.canvas = mCanvas;
-        this.context = this.canvas.getContext("2d");
+        this.context = mCanvas.getContext("2d");
+        this.canvasHeight = mCanvas.height;
+        this.canvasWidth = mCanvas.width;
     }
-    SnakeGame.prototype.start = function () {
-        this.restart();
-        setInterval(function () { return snakeGame.loop(); }, this.gameSpeed);
+    SnakeGame.prototype.clearCanvas = function () {
+        this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     };
-    SnakeGame.prototype.restart = function () {
+    SnakeGame.prototype.pauseGame = function () {
+        //clearInterval(this.game);
+        this.isGamePaused = !this.isGamePaused;
+    };
+    SnakeGame.prototype.start = function () {
+        var _this = this;
+        this.SnakePosition = new Position(100, 100);
+        this.food = new Position(100, 164);
+        setInterval(function () {
+            if (!_this.isGamePaused) {
+                snakeGame.loop();
+            }
+        }, this.gameSpeed);
     };
     SnakeGame.prototype.loop = function () {
+        this.move();
+        if (this.SnakePosition.xPostion == this.food.xPostion && this.SnakePosition.yPosition == this.food.yPosition) {
+            this.eat();
+        }
+        this.checkBorderCollision();
         this.draw();
     };
+    SnakeGame.prototype.eat = function () {
+        console.log("eat");
+    };
+    SnakeGame.prototype.drawApple = function () {
+        this.context.fillStyle = "red";
+        this.context.fillRect(this.food.xPostion, this.food.yPosition, 32, 32);
+    };
     SnakeGame.prototype.draw = function () {
-        console.log("draw");
+        this.clearCanvas();
+        this.drawApple();
         this.context.fillStyle = "green";
-        this.context.fillRect(100, 100, 32, 32);
+        this.context.fillRect(this.SnakePosition.xPostion, this.SnakePosition.yPosition, 32, 32);
+    };
+    SnakeGame.prototype.checkBorderCollision = function () {
+        if (this.SnakePosition.xPostion <= 8) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+        if (this.SnakePosition.xPostion >= this.canvasWidth - 32) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+        if (this.SnakePosition.yPosition >= this.canvasHeight - 32) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+        if (this.SnakePosition.yPosition <= 8) {
+            console.log("dead");
+            this.isGamePaused = true;
+        }
+    };
+    SnakeGame.prototype.move = function () {
+        switch (this.direction) {
+            case Direction.LEFT:
+                this.SnakePosition.xPostion -= 32;
+                break;
+            case Direction.UP:
+                this.SnakePosition.yPosition -= 32;
+                break;
+            case Direction.RIGHT:
+                this.SnakePosition.xPostion += 32;
+                break;
+            case Direction.DOWN:
+                this.SnakePosition.yPosition += 32;
+                break;
+        }
+    };
+    SnakeGame.prototype.moveLeft = function () {
+        if (this.direction != Direction.RIGHT) {
+            this.direction = Direction.LEFT;
+        }
+    };
+    SnakeGame.prototype.moveUp = function () {
+        if (this.direction != Direction.DOWN) {
+            this.direction = Direction.UP;
+        }
+    };
+    SnakeGame.prototype.moveRight = function () {
+        if (this.direction != Direction.LEFT) {
+            this.direction = Direction.RIGHT;
+        }
+    };
+    SnakeGame.prototype.moveDown = function () {
+        if (this.direction != Direction.UP) {
+            this.direction = Direction.DOWN;
+        }
     };
     return SnakeGame;
 }());
-function direction(event) {
-    var key = event.keyCode;
+function keyPressed(event) {
+    var KEYBOARD_BUTTON = event.keyCode;
     var KEY_LEFT = 37;
     var KEY_UP = 38;
     var KEY_RIGHT = 39;
     var KEY_DOWN = 40;
-    switch (key) {
+    var KEY_STOP_GAME = 80;
+    switch (KEYBOARD_BUTTON) {
         case KEY_RIGHT: {
-            console.log("right");
+            snakeGame.moveRight();
             break;
         }
         case KEY_DOWN: {
-            console.log("down");
+            snakeGame.moveDown();
             break;
         }
         case KEY_LEFT: {
-            console.log("left");
+            snakeGame.moveLeft();
             break;
         }
         case KEY_UP: {
-            console.log("up");
+            snakeGame.moveUp();
+            break;
+        }
+        case KEY_STOP_GAME: {
+            snakeGame.pauseGame();
             break;
         }
     }
 }
 window.onload = function () {
-    document.addEventListener("keydown", direction);
+    document.addEventListener("keydown", keyPressed);
     var canvas = document.getElementById('snakeCanvas');
     snakeGame = new SnakeGame(canvas);
-    //snakeGame.start();
+    snakeGame.start();
 };
